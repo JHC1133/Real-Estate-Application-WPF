0a343c0b9fc7bf37a422a1e2f68bcdd8b5936a80
JHC1133 <joarhyltencavallius@gmail.com>
2023-09-19 23:24:34 +0200

    56

---
+++ ModernRealEstates/AbstractClasses/Person.cs
@@ -0,0 +1,21 @@
+﻿using Modern_Real_Estates_by_Joar_H_C;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernRealEstates.AbstractClasses
+{
+    internal abstract class Person
+    {
+        protected Address address;
+
+        public Address Address { get => address; set => address = value; }
+
+        public Person(Address adress)
+        {
+            
+        }
+    }
+}
---
+++ ModernRealEstates/Persons/Buyer.cs
@@ -0,0 +1,18 @@
+﻿using Modern_Real_Estates_by_Joar_H_C;
+using ModernRealEstates.AbstractClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernRealEstates.Persons
+{
+    internal class Buyer : Person
+    {
+        public Buyer(Address adress) : base(adress)
+        {
+            this.Address = address;
+        }
+    }
+}
